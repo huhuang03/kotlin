12c1e3d8f8edbf2d148d45b7d73e47789c148f35
Sergej Jaskiewicz <sergej.jaskiewicz@jetbrains.com>
2025-06-23 18:13:16 +0200

    [IR] Don't generate an extra cast in synthetic `equals` in data classes
    The is-check generated before that makes that cast always succeed.
    
    This doesn't affect the JVM target, as implicit casts are always lowered
    to CHECKAST there, but does make the code for the JS and Native targets
    more efficient.
    
    ^KT-54689 Fixed

--- compiler/testData/codegen/box/dataClasses/equals/sameinstance.kt
+++ compiler/testData/codegen/box/dataClasses/equals/sameinstance.kt
@@ -1,3 +1,10 @@
+// FILECHECK_STAGE: CStubs
+
+// CHECK-LABEL: define zeroext i1 @"kfun:A#equals(kotlin.Any?){}kotlin.Boolean"
+// CHECK: call zeroext i1 @IsSubtype
+// CHECK-NOT: call zeroext i1 @IsSubtype
+// CHECK-LABEL: epilogue:
+// ^ Kotlin/Native check
 data class A(val arg: Any? = null)
 
 fun box() : String {
